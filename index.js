560491e1116b83725ad6b1be4ed73c3d782bf916
Felicia <flinder410010@gmail.com>
2021-08-19 10:33:37 +0800

    change port

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// var fs = require('fs')
-var https = require('https');
 // 如果不需要用 https 的話，要改成引用 http 喔
 var http = require('http');
 var socketio = require('socket.io');
@@ -9,51 +7,25 @@ var cors = require('cors');
 var app = express();
 app.use(cors());
 var port = process.env.PORT || 4000;
-//ws
-// const { Server } = require('ws');
-// const wss = new Server({ server });
 
-// wss.on('connection', (ws) => {
-//     console.log('Client connected');
-//     ws.on('close', () => console.log('Client disconnected'));
-// });
-
-//https 的一些設定，如果不需要使用 ssl 加密連線的話，把內容註解掉就好
 var options = {
     // key: fs.readFileSync('這個網域的 ssl key 位置'),
     // cert: fs.readFileSync('這個網域的 ssl fullchain 位置')
 }
-
 //http & socket port
-var server = http.createServer(options);
-server.listen(process.env.PORT || 4040)
+console.log("Server socket api 4000")
+
+var server = http.createServer(app);
+server.listen( port, function () {
+    console.log('API listening on *:' + port);
+} )
 //>V3後會有cors的問題
 var io = socketio(server, {
     cors: {
         origin: '*',
     }
 });
-console.log("Server socket 4040 , api 4000")
 
-//api port
-// var port = process.env.PORT || 4000;
-// app.listen(port, function () {
-//     console.log('API listening on *:' + port);
-// });
-
-// app.get('', function (req, res, next) {
-//     res.json({ msg: 'Home' })
-// })
-
-// app.get('/products', function (req, res, next) {
-//     res.json({ msg: 'This is CORS-enabled for all origins!' })
-// })
-
-// //用 api 方式取得
-// app.get('/api/messages', function (req, res) {
-//     let messages = 'hellow world'
-//     res.send(messages);
-// })
 
 let messages = [
     { name: "Majer", message: "Welcome!" }
