a34ec03cffd0dff6bae84cc1be0e243de590b685
Felicia <flinder410010@gmail.com>
2021-10-14 11:19:27 +0800

    Update index.js
    綁住cors

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ var express = require('express');
 var cors = require('cors');
 var app = express();
 app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", " * ");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 var port = process.env.PORT || 4010;
 
 var server = http.createServer(app);
