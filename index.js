03b2c716ec1162962f868e5cb09fcd5a2610c314
felicia1910 <43741409+felicia1910@users.noreply.github.com>
2021-11-01 16:10:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -130,11 +130,13 @@ io.on('connection', function (socket) {
             if(mes.user){
                 //已經有這客人
                 if(findCus !== -1){
-                    dataVeiw.chat[2].customer[findCus].chating.push(pushChat(mes, mes.user));
+                    console.log('??？？？',pushChat(mes, mes.user));
+                    let getMes=pushChat(mes, mes.user);
+                    dataVeiw.chat[2].customer[findCus].chating.push(getMes);
                     io.emit("newMessage", {
                         isNew: false,
-                        mes: pushChat(mes, mes.user),
-                        data: dataVeiw.chat[0].customer[findCus]
+                        mes: getMes,
+                        data: dataVeiw.chat[2].customer[findCus]
                     });  
                 }
                 //沒有該客人
@@ -161,6 +163,7 @@ io.on('connection', function (socket) {
                 }
             }else{
                 dataVeiw.chat[2].customer[findCus].chating.push(pushChat(mes, mes.user));
+                console.log('??',pushChat(mes, mes.user));
                 io.emit("newMessage", {
                     isNew: false,
                     mes: pushChat(mes, mes.user),
